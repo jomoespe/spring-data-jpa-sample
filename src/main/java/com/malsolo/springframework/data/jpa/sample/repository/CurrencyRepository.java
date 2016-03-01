7d7b856edcc51c0aac1de08a35cb456dfd034c12
Jose Moreno <jomoespe@gmail.com>
2016-03-01 23:40:27 +0100

    Add TOP and FIRST flters to Currency repository

--- src/main/java/com/malsolo/springframework/data/jpa/sample/repository/CurrencyRepository.java
+++ src/main/java/com/malsolo/springframework/data/jpa/sample/repository/CurrencyRepository.java
@@ -1,6 +1,10 @@
 package com.malsolo.springframework.data.jpa.sample.repository;
 
+import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurrencyRepository extends JpaRepository<Currency, String> {
+    List<Currency> findTop3ByNum(Integer num);
+    List<Currency> findFirst2ByNum(Integer num, Sort sort);
 }
--- src/test/java/com/malsolo/springframework/data/jpa/sample/repository/CurrencyRepositoryIT.java
+++ src/test/java/com/malsolo/springframework/data/jpa/sample/repository/CurrencyRepositoryIT.java
@@ -11,23 +11,39 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
 
 import com.malsolo.springframework.data.jpa.sample.ApplicationConfiguration;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.springframework.data.domain.Sort;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationConfiguration.class)
 public class CurrencyRepositoryIT {
 
-    @Autowired CurrencyRepository currencyRepository;
-    @Autowired JdbcTemplate       jdbcTemplate;
+    @Autowired private CurrencyRepository currencyRepository;
+    @Autowired private JdbcTemplate       jdbcTemplate;
 
-    @Test
-    public void testMarkerMethod() {
-    }
-
+    
     @Test
     public void testSave() {
-        this.currencyRepository.save(new Currency("EUR", 978, 2, "Euro"));
-        this.currencyRepository.flush();
+        currencyRepository.save(new Currency("EUR", 978, 2, "Euro"));
+        currencyRepository.flush();
 
         assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "currency"));
+
+        currencyRepository.save(new Currency("USD", 978, 2, "Euro"));
+        currencyRepository.save(new Currency("GBP", 978, 2, "Euro"));
+        currencyRepository.save(new Currency("JPY", 978, 2, "Euro"));
+        currencyRepository.flush();
+
+        assertEquals(4, JdbcTestUtils.countRowsInTable(jdbcTemplate, "currency"));
+
+        List<Currency> top3 = currencyRepository.findTop3ByNum(978);
+        assertEquals(3, top3.size());
+        top3.forEach((c) -> System.out.printf("!!! currency code=[%s]    \n", c.getCode()));
+
+        List<Currency> last2 = currencyRepository.findFirst2ByNum(978, new Sort(Sort.Direction.ASC, "num"));
+        assertEquals(2, last2.size());
+        last2.forEach((c) -> System.out.printf("*** currency code=[%s]    \n", c.getCode()));
     }
 }
