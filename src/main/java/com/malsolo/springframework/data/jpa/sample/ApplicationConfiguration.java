734ddec28327f7a4191e6c1ef820d9515e68caed
Javier Beneito Barquero <jbbarquero@gmail.com>
2016-03-01 09:12:22 +0100

    SPRING-DATA-JPA-SAMPLE-0002: test added.
    Now witness the power of this fully tested and operational application

--- src/main/java/com/malsolo/springframework/data/jpa/sample/ApplicationConfiguration.java
+++ src/main/java/com/malsolo/springframework/data/jpa/sample/ApplicationConfiguration.java
@@ -2,22 +2,24 @@ package com.malsolo.springframework.data.jpa.sample;
 
 import static org.springframework.orm.jpa.vendor.Database.H2;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManagerFactory;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.malsolo.springframework.data.jpa.sample.repository.Currency;
 
 @Configuration
-//@EnableTransactionManagement
 @EnableJpaRepositories(basePackageClasses = Currency.class)
 public class ApplicationConfiguration {
 
@@ -33,18 +35,27 @@ public class ApplicationConfiguration {
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
 		adapter.setDatabase(H2);
 		adapter.setShowSql(true);
-		adapter.setGenerateDdl(false);
-		adapter.setDatabasePlatform("org.hibernate.dialect.HSQLDialect");
+		adapter.setGenerateDdl(true);
 		return adapter;
 	}
 
 	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
-			JpaVendorAdapter jpaVendorAdapter) {
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean emfb = new LocalContainerEntityManagerFactoryBean();
-		emfb.setDataSource(dataSource);
-		emfb.setJpaVendorAdapter(jpaVendorAdapter);
+		emfb.setDataSource(dataSource());
+		emfb.setJpaVendorAdapter(jpaVendorAdapter());
 		emfb.setPackagesToScan(Currency.class.getPackage().getName());
 		return emfb;
 	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(entityManagerFactory().getObject());
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
+	}
+
 }
--- src/test/java/com/malsolo/springframework/data/jpa/sample/repository/CurrencyRepositoryTest.java
+++ src/test/java/com/malsolo/springframework/data/jpa/sample/repository/CurrencyRepositoryTest.java
@@ -1,13 +1,38 @@
 package com.malsolo.springframework.data.jpa.sample.repository;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
 
 import com.malsolo.springframework.data.jpa.sample.ApplicationConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ApplicationConfiguration.class)
 public class CurrencyRepositoryTest {
+	
+	@Autowired
+	CurrencyRepository currencyRepository;
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Test
+	public void testMarkerMethod() {
+	}
+
+	@Test
+	public void testSave() {
+		
+		this.currencyRepository.save(new Currency("EUR", 978, 2, "Euro"));
+		
+		this.currencyRepository.flush();
+		
+		assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "currency"));
+	}
 }
